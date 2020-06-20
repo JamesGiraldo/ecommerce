401a1bbdeaa222eb5c7363375a813ecb99432102
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-06-20 00:21:21 -0500

    lindoo :v el panel de administracion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_233742) do
+ActiveRecord::Schema.define(version: 2020_06_20_010937) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "c_img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
